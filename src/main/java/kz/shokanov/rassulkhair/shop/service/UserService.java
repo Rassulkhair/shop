7e8f75347aaf5022e5db02e230b2d9c1687418da
Rassulkhair <rasulhair@gmail.com>
2023-04-09 21:21:41 +0600

    method for spring security

--- src/main/java/kz/shokanov/rassulkhair/shop/service/UserService.java
+++ src/main/java/kz/shokanov/rassulkhair/shop/service/UserService.java
@@ -4,13 +4,16 @@ import jakarta.transaction.Transactional;
 import kz.shokanov.rassulkhair.shop.entity.enumiration.Role;
 import kz.shokanov.rassulkhair.shop.repository.UserRepo;
 import kz.shokanov.rassulkhair.shop.entity.User;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
@@ -32,5 +35,11 @@ public class UserService{
         user.setPassword(passwordEncoder.encode(password));
         user.setRole(Role.USER);
         userRepo.save(user);
+    }
+
+    public User getCurrentUser() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication authentication = context.getAuthentication();
+        return userRepo.findUserByName(authentication.getName());
     }
 }
